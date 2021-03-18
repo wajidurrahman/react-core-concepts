a35cd52841ca7490694bd49c0553645a9ef24c3d
Wajidur Rahman <wrfuad0@gmail.com>
2021-03-18 23:05:17 +0600

    react practice

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ const nayoks = ['Anwar', 'Jafor', 'Alomgir', 'Salam']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am a React Person</p>
+        <Person name={nayoks[0]} nayika="Moushumi"></Person>
+        <Person name="BappaRaz" nayika="Chaka"></Person>
+        <Person name="Jasim" nayika="Shabana"></Person>
+        <Person name="Elias K" nayika="Bobita"></Person>
       </header>
     </div>
+  );
+}
+
+function Person(props){
+  const personStyle ={
+    border:'2px solid gold',
+    width:'400px',
+    margin:'10px'
+  }
+  return (
+  <div style={personStyle}>
+    <h1>Nayok: {props.name}</h1>
+    <h3>Hero of {props.nayika}</h3>
+  </div>
   );
 }
 
